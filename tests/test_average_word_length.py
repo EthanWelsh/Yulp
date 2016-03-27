f38ea1eb3d8306ab7cfa16e17c0cf151ace5bdf5
Ethan Welsh <welshej@gmail.com>
2016-03-27 11:23:30 -0700

    add sample feature and associated test

---
+++ tests/test_average_word_length.py
@@ -0,0 +1,12 @@
+import pytest
+from features.average_word_length import AverageWordLength
+
+
+class TestAverageWordLength:
+
+    def test_it_fails_without_passing_text(self):
+        with pytest.raises(TypeError):
+            AverageWordLength().score()
+
+    def test_returns_a_number(self):
+        assert isinstance(AverageWordLength().score([['test']]), float)
