9fa1779eab22bbea6aad1d31386a8c26b691aa8b
Ethan Welsh <welshej@gmail.com>
2016-03-27 11:57:35 -0700

    feature vector test

--- tests/test_feature_vector.py
+++ tests/test_feature_vector.py
@@ -1,10 +1,43 @@
 import pytest
+from model.feature import FeatureVector, Feature
 
 
-class TestFeature:
-    def test_it_fails_without_passing_text(self):
+class TestFeatureVector:
+    class BogusFeature(Feature):
+        def load(self, path):
+            pass
+
+        def train(self, data, labels):
+            pass
+
+        def score(self, data):
+            return 0
+
+        def save(self, path):
+            pass
+
+    def test_append(self):
+        FeatureVector().append(TestFeatureVector.BogusFeature())
+
+    def test_fail_on_append_non_feature(self):
         with pytest.raises(TypeError):
-            AverageWordLength().score()
+            FeatureVector().append(0)
 
-    def test_returns_a_number(self):
-        assert isinstance(AverageWordLength().score([['test']]), float)
+    def test_train(self):
+
+        try:
+
+            fv = FeatureVector()
+            fv.append(TestFeatureVector.BogusFeature())
+            fv.append(TestFeatureVector.BogusFeature())
+            fv.append(TestFeatureVector.BogusFeature())
+            fv.train([['hello', 'world'], ['this', 'is', 'me']], labels=[])
+        except:
+            pytest.fail('Training of features failed')
+
+    def test_score(self):
+        fv = FeatureVector()
+        fv.append(TestFeatureVector.BogusFeature())
+        fv.append(TestFeatureVector.BogusFeature())
+        fv.append(TestFeatureVector.BogusFeature())
+        assert fv.score([[['A']], [['B']], [['C']], [['D']]]) == [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
