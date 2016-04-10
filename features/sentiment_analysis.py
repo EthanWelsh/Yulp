efe742e90ae3f53e290e9418910d8f6efbf00970
Ethan Welsh <welshej@gmail.com>
2016-04-10 00:17:38 -0400

    add main with examples

--- features/sentiment_analysis.py
+++ features/sentiment_analysis.py
@@ -10,3 +10,9 @@ class SentimentAnalysis(Feature):
     def score(self, text):
         scores = self.intensity_analyzer.polarity_scores(text)
         return scores['compound']
+
+
+if __name__ == '__main__':
+    sa = SentimentAnalysis()
+    print(sa.score('I really hate this place'))
+    print(sa.score('Such a good place to grab a meal!'))
