622199e1886d6cfe03f51b9010211627cb2fb4f0
Ethan Welsh <welshej@gmail.com>
2016-04-10 00:38:07 -0400

    minor fixes in GrammarScore feature

--- features/grammar_analysis.py
+++ features/grammar_analysis.py
@@ -1,37 +1,55 @@
-import re, collections
+import collections
+import re
+from model.feature import Feature
+import string
 
-#This function returns a value between 0 and 1 representing the grammatical correctness of the input text
-def spelling_analysis(text):
-    return 0
 
-import re, collections
+class GrammarScore(Feature):
+    def train(features):
+        model = collections.defaultdict(lambda: 1)
+        for f in features:
+            model[f] += 1
+        return model
 
-def words(text):
-    return re.findall('[a-z]+', text.lower())
+    @staticmethod
+    def spelling_analysis(text):
+        """
+        This function returns a value between 0 and 1 representing the grammatical correctness of the input text
+        """
+        return 0
 
-def train(features):
-    model = collections.defaultdict(lambda: 1)
-    for f in features:
-        model[f] += 1
-    return model
+    @staticmethod
+    def words(text):
+        return re.findall('[a-z]+', text.lower())
 
-NWORDS = train(words(file('/yulp/data/train_data/bigGutenbergSample.txt').read()))
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    @staticmethod
+    def edits1(word):
+        s = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+        deletes = [a + b[1:] for a, b in s if b]
+        transposes = [a + b[1] + b[0] + b[2:] for a, b in s if len(b) > 1]
+        replaces = [a + c + b[1:] for a, b in s for c in alphabet if b]
+        inserts = [a + c + b for a, b in s for c in alphabet]
+        return set(deletes + transposes + replaces + inserts)
 
-def edits1(word):
-    s = [(word[:i], word[i:]) for i in range(len(word) + 1)]
-    deletes    = [a + b[1:] for a, b in s if b]
-    transposes = [a + b[1] + b[0] + b[2:] for a, b in s if len(b)>1]
-    replaces   = [a + c + b[1:] for a, b in s for c in alphabet if b]
-    inserts    = [a + c + b     for a, b in s for c in alphabet]
-    return set(deletes + transposes + replaces + inserts)
+    @staticmethod
+    def known_edits2(word):
+        return set(e2 for e1 in GrammarScore.edits1(word) for e2 in GrammarScore.edits1(e1) if e2 in NWORDS)
 
-def known_edits2(word):
-    return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
+    @staticmethod
+    def known(words):
+        return set(w for w in words if w in NWORDS)
 
-def known(words):
-    return set(w for w in words if w in NWORDS)
+    @staticmethod
+    def correct(word):
+        candidates = GrammarScore.known([word]) or \
+                     GrammarScore.known(GrammarScore.edits1(word)) or \
+                     GrammarScore.known_edits2(word) or [word]
+        return max(candidates, key=NWORDS.get)
 
-def correct(word):
-    candidates = known([word]) or known(edits1(word)) or    known_edits2(word) or [word]
-    return max(candidates, key=NWORDS.get)
+
+if __name__ == '__main__':
+    gs = GrammarScore()
+    alphabet = string.ascii_lowercase
+
+    with open('/yulp/data/train_data/bigGutenbergSample.txt') as data:
+        NWORDS = gs.train(gs.words(data.read()))
