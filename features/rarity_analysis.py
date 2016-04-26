96eba239815b2051b75ccf5d94d1bc99399058c8
Ethan Welsh <welshej@gmail.com>
2016-04-26 10:44:29 -0400

    code review & fix a few simple problems

--- features/rarity_analysis.py
+++ features/rarity_analysis.py
@@ -1,44 +1,60 @@
-#dictionary source: Princeton University "About WordNet." WordNet. Princeton University. 2010. <http://wordnet.princeton.edu>
-import re, collections
+import re
+
 from model.feature import Feature
+
+"""
+dictionary source: Princeton University "About WordNet." WordNet. Princeton University. 2010.
+<http://wordnet.princeton.edu>
+
+"""
+
+
 class Rarity(Feature):
-
-
     def __init__(self):
         self.word_counts = {}
         self.max_word_occurrences = 0
 
-    #This function returns a value between 0 and 1 representing the average rarity value per word
     def rarity_analysis(self, text):
+        """
+        :return: a value between 0 and 1 representing the average rarity value per word
+        """
+
         input_len = 0
-        total_sent_occurrence_count = 0
+        total_sent_count = 0
+
         for sent in text:
             input_len += len(sent)
             for word in sent:
                 if word in self.word_counts:
-                    total_sent_occurrence_count += self.word_counts[word]
+                    total_sent_count += self.word_counts[word]
 
-        return (total_sent_occurrence_count / input_len) / self.max_word_occurrences if self.max_word_occurrences > 0 else 0.0
-        #return -1
+        return (total_sent_count / input_len) / self.max_word_occurrences if self.max_word_occurrences > 0 else 0.0
 
-
-    #this function returns list with every word(in lower case) in the argument containing a-z
-    def words(self, text):
+    @staticmethod
+    def words(text):
+        """
+        :return: list with every word(in lower case) in the argument containing a-z
+        """
         return re.findall('[a-z]+', text.lower())
 
-    #this function returns a model with each features unique word's number of occurrences
     def train(self):
-        #First we'll put in every word in the dictionary
-        dictionaryFile = open('../data/train_data/dictionary.txt', 'r')
-        dictionary = rarity.words(dictionaryFile.read())
+        """
+        :return: a model with each features unique word's number of occurrences
+        """
+
+        # First we'll put in every word in the dictionary
+        with open('data/train_data/dictionary.txt', 'r') as dictionary_file:
+            dictionary = self.words(dictionary_file.read())
+
         for dict_entry in dictionary:
             if dict_entry not in self.word_counts:
                 self.word_counts[dict_entry] = 0
             self.word_counts[dict_entry] += 1
 
         # now we train a bit with actual text for word occurrence numbers
-        trainFile = open('../data/train_data/bigGutenbergSample.txt', 'r')
-        training_corpus = self.words(trainFile.read())
+        with open('data/train_data/bigGutenbergSample.txt', 'r', encoding='utf-8') as train_file:
+            training_corpus = self.words(train_file.read())
+
         for word in training_corpus:
             if word not in self.word_counts:
                 self.word_counts[word] = 0
@@ -47,11 +63,12 @@ class Rarity(Feature):
                 self.max_word_occurrences = self.word_counts[word]
 
     def score(self, data):
-        return Rarity.rarity_analysis(self, data)
+        return self.rarity_analysis(data)
 
 
-#this only happens when we run from the script
 if __name__ == '__main__':
     rarity = Rarity()
     rarity.train()
-    print(rarity.score(["This sentence has sumthing misspelled in it".split(), "This is just another sentence".split()]))
+    print(rarity.score([
+        "This sentence has sumthing misspelled in it".split(),
+        "This is just another sentence".split()]))
