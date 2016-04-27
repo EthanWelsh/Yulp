456aae8362e02b475c7c361240426584fcc68acb
Ethan Welsh <welshej@gmail.com>
2016-04-26 23:48:52 -0400

    decompose kbest and form single scalar

--- features/tfidf.py
+++ features/tfidf.py
@@ -2,6 +2,7 @@ from itertools import chain
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.decomposition import TruncatedSVD
 from model.feature import Feature
 
 from main import retrieve_reviews
@@ -12,6 +13,7 @@ class TfIdf(Feature):
     def __init__(self):
         self.kbest = None
         self.vect = None
+        self.truncated = None
 
     def train(self, reviews, labels):
         self.vect = TfidfVectorizer(analyzer='word', ngram_range=(1, 2), stop_words='english')
@@ -19,21 +21,25 @@ class TfIdf(Feature):
         reviews_text = [' '.join(list(chain.from_iterable(review))) for review in reviews]
         tfidf_matrix = self.vect.fit_transform(reviews_text).toarray()
 
-        self.kbest = SelectKBest(chi2, k=5).fit(tfidf_matrix, labels)
+        self.truncated = TruncatedSVD(n_components=1).fit(tfidf_matrix)
+
+        self.kbest = SelectKBest(chi2, k=1).fit(self.truncated.transform(tfidf_matrix), labels)
 
     def score(self, data):
         reviews_text = ' '.join(list(chain.from_iterable(data)))
         tfidf_matrix = self.vect.transform([reviews_text]).toarray()
-        print(self.kbest.transform(tfidf_matrix))
+        self.kbest.transform(self.truncated.transform(tfidf_matrix))
+
+        return self.kbest.transform(self.truncated.transform(tfidf_matrix))[0, 0]
+
 
 if __name__ == '__main__':
-
     reviews = retrieve_reviews(100, data_path='../data/reviews.json')
     reviews, labels = zip(*reviews)
     feat = TfIdf()
 
     feat.train(reviews, labels)
-    feat.score(reviews[0])
+    print(feat.score(reviews[0]))
 
 
 
