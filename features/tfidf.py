936e1297c0010e302f748603adb97c9ac2d98ce9
Ethan Welsh <welshej@gmail.com>
2016-04-26 23:28:22 -0400

    progress on Tfidf feature

--- features/tfidf.py
+++ features/tfidf.py
@@ -1,24 +1,40 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
+from itertools import chain
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectKBest, chi2
 from model.feature import Feature
+
+from main import retrieve_reviews
 
 
 class TfIdf(Feature):
-    def train(self):
-        pass
+
+    def __init__(self):
+        self.kbest = None
+        self.vect = None
+
+    def train(self, reviews, labels):
+        self.vect = TfidfVectorizer(analyzer='word', ngram_range=(1, 2), stop_words='english')
+
+        reviews_text = [' '.join(list(chain.from_iterable(review))) for review in reviews]
+        tfidf_matrix = self.vect.fit_transform(reviews_text).toarray()
+
+        self.kbest = SelectKBest(chi2, k=5).fit(tfidf_matrix, labels)
 
     def score(self, data):
-        pass
-
+        reviews_text = ' '.join(list(chain.from_iterable(data)))
+        tfidf_matrix = self.vect.transform([reviews_text]).toarray()
+        print(self.kbest.transform(tfidf_matrix))
 
 if __name__ == '__main__':
-    vect = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), stop_words='english')
 
-    tfidf_matrix = vect.fit_transform([open('/Users/welshej/Desktop/CS1653/lec.org').read(),
-                                       open('/Users/welshej/Desktop/PHYS87/lec.org').read()])
+    reviews = retrieve_reviews(100, data_path='../data/reviews.json')
+    reviews, labels = zip(*reviews)
+    feat = TfIdf()
 
-    print(vect.get_feature_names())
-    print(tfidf_matrix.shape)
+    feat.train(reviews, labels)
+    feat.score(reviews[0])
+
 
 
 
