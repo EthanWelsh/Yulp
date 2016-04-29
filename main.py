dbf86008d86ffc705934e4d75b0cad8bd02420b9
Ethan Welsh <welshej@gmail.com>
2016-04-29 15:09:40 -0400

    add two new food features

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from features import average_word_length, sentiment, rarity, tfidf, readability, spelling
+from features import average_word_length, sentiment, rarity, tfidf, readability, spelling, food_sophistication, \
+    sentence_topic
 from model.feature import FeatureVector
 from model.svm import SVM
 from util.parse_reviews import retrieve_reviews
@@ -9,8 +10,8 @@ def main():
     reviews = retrieve_reviews(5000)
 
     # Split reviews into a training and testing portion
-    train_reviews = reviews[:4000]
-    test_reviews = reviews[4001 + 1:]
+    train_reviews = reviews[:4500]
+    test_reviews = reviews[4500 + 1:]
 
     # Separate text and label to use during the training process
     text, labels = zip(*train_reviews)
@@ -18,12 +19,14 @@ def main():
     vector = FeatureVector()
 
     # Add features into feature vector
-    vector.append(average_word_length.AverageWordLength())
     vector.append(sentiment.SentimentAnalysis())
-    vector.append(rarity.Rarity())
     vector.append(tfidf.TfIdf())
     vector.append(readability.Readability())
+    vector.append(food_sophistication.FoodSophistication())
+    vector.append(average_word_length.AverageWordLength())
+    vector.append(rarity.Rarity())
     vector.append(spelling.Spelling())
+    vector.append(sentence_topic.SentenceTopic())
 
     # Train all of the features individually
     vector.train(text, labels)
